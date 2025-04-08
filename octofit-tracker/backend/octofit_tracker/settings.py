aee20de4424f4780147860d311a28b2ff1f1b833
vovoluke <143028112+vovoluke@users.noreply.github.com>
2025-04-08 14:47:24 +0000

    Step 4 done

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-^8ipb1h8lqb-c5cbdu6s6d(1=_%xa0+9sv=kvbr1f7(t=p$xkb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'expert-space-succotash-wr7xg579pv563gg55-8000.app.github.dev']
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -6,7 +6,7 @@ from .models import User, Team, Activity, Leaderboard, Workout
 
 @api_view(['GET'])
 def api_root(request, format=None):
-    base_url = 'http://localhost:8000/'
+    base_url = 'https://expert-space-succotash-wr7xg579pv563gg55-8000.app.github.dev/'
     return Response({
         'users': base_url + 'api/users/',
         'teams': base_url + 'api/teams/',
